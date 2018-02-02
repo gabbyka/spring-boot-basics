1a4e33da0e9dcaaa78d5380f4e71aa3199346404
Nyakas Gábor <Nyakas Gbor@192.168.0.214>
2018-02-02 21:36:08 +0100

    added repositories

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -22,7 +22,14 @@ public class Author {
     @ManyToMany(mappedBy="authors")
     private Set<Book> books = new HashSet<>();
 
-    public Long getId() {
+    
+    public Author(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
         return id;
     }
 
@@ -54,4 +61,34 @@ public class Author {
         this.books = books;
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
+	}
+    
 }
