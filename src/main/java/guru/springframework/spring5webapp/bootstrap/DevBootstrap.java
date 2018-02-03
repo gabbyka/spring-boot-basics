a8215dd75b2d1de36bd14abac5d16f2318ebf29a
Nyakas Gábor <Nyakas Gbor@192.168.0.214>
2018-02-03 22:11:07 +0100

    authors page added

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
 import guru.springframework.spring5webapp.model.Publisher;
@@ -13,45 +12,45 @@ import guru.springframework.spring5webapp.repositories.BookRepository;
 import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
-public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
-	@Autowired
-	private AuthorRepository authorRepository;
-	
-	@Autowired
-	private BookRepository bookRepository;
-	
-	@Autowired
-	private PublisherRepository publisherRepository;
-	
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent event) {
-		initData();
-	}
-	
-	private void initData() {
-		
-		// Eric
-		Author eric = new Author("Eric", "Evans");
-		Publisher harper = new Publisher("Harper Collis", "somewhere");
-		Book ddd = new Book("Domain Driven Design", "1234", harper);
-		eric.getBooks().add(ddd);
-		ddd.getAuthors().add(eric);
-		
-		publisherRepository.save(harper);
-		authorRepository.save(eric);
-		bookRepository.save(ddd);
-		
-		Author rod = new Author("Rod", "Johnson");
-		Publisher worx = new Publisher("Worx", "anywhere");
-		Book noEJB = new Book("J2EE development without EJB", "23444", worx);
-		rod.getBooks().add(noEJB);
-		
-		publisherRepository.save(worx);
-		authorRepository.save(rod);
-		bookRepository.save(noEJB);
-		
-	}
-	
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        initData();
+    }
+
+    private void initData() {
+
+        // Eric
+        Author eric = new Author("Eric", "Evans");
+        Publisher harper = new Publisher("Harper Collis", "somewhere");
+        Book ddd = new Book("Domain Driven Design", "1234", harper);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        publisherRepository.save(harper);
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Publisher worx = new Publisher("Worx", "anywhere");
+        Book noEJB = new Book("J2EE development without EJB", "23444", worx);
+        rod.getBooks().add(noEJB);
+
+        publisherRepository.save(worx);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+    }
+
 
 }
